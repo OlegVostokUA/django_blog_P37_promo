73dba9bcc47af2f2bec19158b6bf35f9a9313c5e
Oleg <example@gmail.com>
2024-05-02 23:19:07 +0300

    02/05

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Comment
-
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class EmailPostForm(forms.Form):
@@ -19,10 +19,23 @@ class CommentForm(forms.ModelForm):
         fields = ('name', 'email', 'body')
 
 
-
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -12,7 +12,9 @@ from django.db.models import Count
 
 from django.shortcuts import redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 # class PostListView(ListView):
 #     queryset = Post.objects.all()
 #     context_object_name = 'posts'
@@ -109,7 +111,27 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Ваш профіль успішно оновлено.')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
 
 
 def logout_confirm(request):
